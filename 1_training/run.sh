0e205c2cbfc6c9701d82c175e3faaf1c2b6cdca9
frostedoyster <bigi.f@libero.it>
2025-11-21 09:56:09 +0100

    Update last section

--- 1_training/run.sh
+++
@@ -1,5 +0,0 @@
-python generate_structures.py
-mtt train options.yaml
-mtt train options-llpr.yaml -o model-llpr.pt
-mtt eval model-llpr.pt eval.yaml -b 20
-python plot_uncertainties.py
--- 2_fine_tuning/run.sh
+++
@@ -1,6 +0,0 @@
-wget https://huggingface.co/lab-cosmo/pet-mad/resolve/v1.0.2/models/pet-mad-v1.0.2.ckpt
-python generate_structures.py
-mtt train options.yaml
-mtt train options-llpr.yaml -o model-llpr.pt
-mtt eval model-llpr.pt eval.yaml -b 20
-python plot_uncertainties.py
